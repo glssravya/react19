ff0dd01d2f12c56fcdd98bc0187c166f3bdc3b8c
Sravya GLS <glssravya@gmail.com>
2025-06-13 06:35:30 +0530

    fix: Adding react

--- App.js
+++ App.js
@@ -1,43 +1,20 @@
-{/* <div id="parent">
-    <div id="child">
-        <h1 id="heading"></h1>
-    </div>
-</div> */}
-
-// const htmlStructure = React.createElement("div",{id:"parent"},
-//     React.createElement("div",{id:"child"},React.createElement("h1", 
-//         { id: "heading" },
-//     "Hello world from React")));
-
-
-{/* 
-    <div id="parent">
-        <div id="child1">
-            <h1>Im h1 tag</h1>
-            <h2>Im h2 tag</h2>
-        </div>
-        <div id="child2">
-            <h1>Im h1 tag</h1>
-            <h2>Im h2 tag</h2>
-        </div>
-    </div> 
-*/}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const htmlStructure = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},
+    React.createElement("div",{id:"child1",key:"child1"},
         [
-         React.createElement("h1", { id: "heading" },"Im h1 tag"),
-         React.createElement("h2", { id: "heading" },"Im h2 tag")
+         React.createElement("h1", { id: "heading",key: "child1h1" },"Im h1 tag"),
+         React.createElement("h2", { id: "heading2",key: "child1h2" },"Im h2 tag")
         ]
     ),
-    React.createElement("div",{id:"child2"},
+    React.createElement("div",{id:"child2",key:"child2"},
         [
-         React.createElement("h1", { id: "heading" },"Im h1 tag"),
-         React.createElement("h2", { id: "heading" },"Im h2 tag")
+         React.createElement("h1", { id: "heading3",key: "child2h1" },"Im h1 tag"),
+         React.createElement("h2", { id: "heading4",key:"child2h2" },"Im h2 tag")
         ]
     )
 ]);
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(htmlStructure);
